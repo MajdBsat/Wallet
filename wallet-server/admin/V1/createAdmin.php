a84bd42cb745a9d51e23cbc81191448961ac92a9
MajdBsat <majdalbsat@gmail.com>
2025-03-04 10:06:14 +0200

    created an admin signin and admin create apis

---
+++ wallet-server/admin/V1/createAdmin.php
@@ -0,0 +1,29 @@
+<?php
+
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST, OPTIONS");
+header("Content-Type: application/json");
+header("Access-Control-Allow-Headers: Content-Type");
+
+include("../../connection/connection.php");
+include("../../models/Admin.php");
+
+$data = json_decode(file_get_contents("php://input"), true);
+
+$admin = new Admin($conn);
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    if (isset($data["email"], $data["phone_number"], $data["password"])) {
+        $email = trim($data["email"]);
+        $phoneNumber = trim($data["phone_number"]);
+        $password = trim($data["password"]);
+        
+        echo $admin->signUp($email, $phoneNumber, $password);
+    } else {
+        echo json_encode(["status" => "error", "message" => "Email, phone number, and password are required."]);
+    }
+} else {
+    echo json_encode(["status" => "error", "message" => "Invalid request method."]);
+}
+
+?>
